e7607c3fe837e44fdf1552e3440dc1c24e094840
Negrete, Diego <Diego.Negrete@united.com>
2024-01-15 13:55:57 -0600

    lint

--- src/resources/data/Paths.tsx
+++ src/resources/data/Paths.tsx
@@ -1,12 +1,6 @@
+const HOME_PATH_ROOT = "/";
+const PERCIPIO_PAGE = "https://apex.percipio.com/login?state=%2F#/";
+const IC_POINTS_PAGE = "https://icptracker.apexsystems.com/";
+const TIME_SHEET_PAGE = "https://hrms.apexsystemsinc.com/psp/HRPRD/?cmd=login";
 
-const HOME_PATH_ROOT = '/';
-const PERCIPIO_PAGE= 'https://apex.percipio.com/login?state=%2F#/';
-const ICPOINTS_PAGE= 'https://icptracker.apexsystems.com/';
-const TIME_SHEET_PAGE= 'https://hrms.apexsystemsinc.com/psp/HRPRD/?cmd=login';
-
-export {
-    HOME_PATH_ROOT,
-    PERCIPIO_PAGE,
-    ICPOINTS_PAGE,
-    TIME_SHEET_PAGE,
-}
+export { HOME_PATH_ROOT, PERCIPIO_PAGE, IC_POINTS_PAGE, TIME_SHEET_PAGE };
