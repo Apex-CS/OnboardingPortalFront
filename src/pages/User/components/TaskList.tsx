1b030e0b52e11b1ad2709230169d2af39f0a8963
Negrete, Diego <Diego.Negrete@united.com>
2024-01-15 15:13:13 -0600

    Add handler error for TaskList Component

--- src/pages/User/components/TaskList.tsx
+++ src/pages/User/components/TaskList.tsx
@@ -3,6 +3,7 @@ import { useFetch } from "../../../hooks/useFetch";
 import { URL_TASK_COUNT } from "../../../resources/data/APIPath";
 import { TaskElementProps, TaskListElementProps } from "../../../types/types";
 import { getRandomNumber } from "../../../utils/utils";
+import { ErrorMessage } from "../../../components";
 const TaskElement = ({ item }: TaskElementProps) => {
   const tempClass = `text-md flex w-1/5 items-center justify-center text-zinc-200 mx-2 rounded-xl px-2 py-1`;
   const total = item.total;
@@ -34,10 +35,11 @@ const TaskElement = ({ item }: TaskElementProps) => {
   );
 };
 const TaskList = () => {
-  const { data: dataTaskCount, fetchData: fetchDataTaskCount } = useFetch(
-    URL_TASK_COUNT,
-    "GET"
-  );
+  const {
+    data: dataTaskCount,
+    fetchData: fetchDataTaskCount,
+    error: errorData,
+  } = useFetch(URL_TASK_COUNT, "GET");
 
   const [listTaskDataCount, setListTaskDataCount] = useState<
     TaskListElementProps[]
@@ -62,12 +64,19 @@ const TaskList = () => {
     }
   }, [dataTaskCount]);
   return (
-    <div className="flex h-60 flex-col border rounded-t-lg border-zinc-300">
-      <ul className="mx-1 bg-white rounded-t-lg divide-y divide-zinc-200 px-2">
-        {listTaskDataCount.map((item) => (
-          <TaskElement item={item} />
-        ))}
-      </ul>
+    <div className="flex h-60 w-[340px] items-center justify-center flex-col border rounded-t-lg border-zinc-300">
+      {errorData === null ? (
+        <ul className="mx-1 bg-white rounded-t-lg divide-y divide-zinc-200 px-2">
+          {listTaskDataCount.map((item) => (
+            <TaskElement item={item} />
+          ))}
+        </ul>
+      ) : (
+        <ErrorMessage
+          description={errorData?.message}
+          title={errorData?.name}
+        />
+      )}
     </div>
   );
 };
